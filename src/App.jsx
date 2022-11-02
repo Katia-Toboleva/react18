d9a87121adf4cb701cb4937a3621d687589ce8e5
Katia <katia.toboleva@and.digital>
2022-11-02 18:35:51 +0000

    setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://ucarecdn.com/d5503b06-ce10-4651-8b9b-d5c3bb7249ad/" className="App-logo" alt="logo" />
+        <p>
+          Simple React 18 App.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
